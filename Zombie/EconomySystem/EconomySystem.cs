d3d72dc042d0e4eea25b82196c968101f25d8f24
Hamish <hamish.zou@stu.hunau.edu.cn>
2022-06-10 21:50:07 +0800

    create EconomySystem.cs, as economy system main class

---
+++ Zombie/EconomySystem/EconomySystem.cs
@@ -0,0 +1,34 @@
+﻿#region << File Comment >>
+
+/*----------------------------------------------------------------
+// Author：Hamish Chengcheng Zou, hamish.zou@stu.hunau.edu.cn
+// Date&Time：10 Jun 2022, Friday @19:06 GMT+8
+//===============================================================
+// All scripts in this field is basically for course experiment use
+// It may direct problems if use outside specific environment
+// **NO** reference value
+//----------------------------------------------------------------*/
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace Zombie.EconomySystem
+{
+    public enum EcoEventType
+    {
+        AutoRaise,
+        CreateBotany
+    }
+    class EconomySystem : IGameSystem
+    {
+        private Dictionary<EcoEventType, ISubject> _ecoEvents = new Dictionary<EcoEventType, ISubject>();
+
+        public override void Init()
+        {
+            base.Init();
+        }
+        
+        
+    }
+}
