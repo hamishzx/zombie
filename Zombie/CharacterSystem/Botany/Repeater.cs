6ea5d72b3af3ce9bc6736bc8082e23af87ba4932
Hamish Chengcheng Zou <hamish.zou@stu.hunau.edu.cn>
2022-06-13 23:45:30 +0800

    edit file head to comply with rule

--- Zombie/CharacterSystem/Botany/Repeater.cs
+++ Zombie/CharacterSystem/Botany/Repeater.cs
@@ -1,13 +1,13 @@
 ﻿#region << File Comment >>
 
-/*----------------------------------------------------------------
-// Author：Hamish Chengcheng Zou, hamish.zou@stu.hunau.edu.cn
-// Date&Time：10 Jun 2022, Friday @00:59 GMT+8
-//===============================================================
-// All scripts in this field is basically for course experiment use
-// It may direct problems if use outside specific environment
-// **NO** reference value
-//----------------------------------------------------------------*/
+/* ----------------------------------------------------------------
+ * Author：Hamish Chengcheng Zou, hamish.zou@stu.hunau.edu.cn
+ * Date&Time：10 Jun 2022, Friday @00:59 GMT+8
+ * ===============================================================
+ * All scripts in this field is basically for course experiment use
+ * It may direct problems if use outside specific environment
+ * **NO** reference value
+ * ----------------------------------------------------------------*/
 
 #endregion
 
--- Zombie/Subscribe/Observer/NewStageObserverStageSystem.cs
+++ Zombie/Subscribe/Observer/NewStageObserverStageSystem.cs
@@ -1,13 +1,13 @@
 ﻿#region << File Comment >>
 
-/*----------------------------------------------------------------
-// Author：Hamish Chengcheng Zou, hamish.zou@stu.hunau.edu.cn
-// Date&Time：09 Jun 2022, Thursday @12:47 GMT+8
-//===============================================================
-// All scripts in this field is basically for course experiment use
-// It may direct problems if use outside specific environment
-// **NO** reference value
-//----------------------------------------------------------------*/
+/* ----------------------------------------------------------------
+ * Author：Hamish Chengcheng Zou, hamish.zou@stu.hunau.edu.cn
+ * Date&Time：09 Jun 2022, Thursday @12:47 GMT+8
+ * ===============================================================
+ * All scripts in this field is basically for course experiment use
+ * It may direct problems if use outside specific environment
+ * **NO** reference value
+ * ----------------------------------------------------------------*/
 
 #endregion
 
