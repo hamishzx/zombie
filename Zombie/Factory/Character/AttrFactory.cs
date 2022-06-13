3222ee5f46eb8a35bd7c40bb9bf3a18de606b16b
Hamish Chengcheng Zou <hamish.zou@stu.hunau.edu.cn>
2022-06-13 23:45:18 +0800

    append attr for Repeater and ThreePeashooter

--- Zombie/Factory/Character/AttrFactory.cs
+++ Zombie/Factory/Character/AttrFactory.cs
@@ -18,6 +18,8 @@ namespace Zombie
             mCharacterBaseAttrDict.Add(typeof(EnemyZombie), new CharacterBaseAttr("丧尸", 100, 2, 10, 20));
             mCharacterBaseAttrDict.Add(typeof(EnemyConeheadZombie), new CharacterBaseAttr("路障僵尸", 100, 3, 15, 30));
             mCharacterBaseAttrDict.Add(typeof(BotanicRepeater), new CharacterBaseAttr("豌豆射手", 100, 2, 5, 25));   
+            mCharacterBaseAttrDict.Add(typeof(Repeater), new CharacterBaseAttr("双发射手", 150, 2, 5, 30));
+            mCharacterBaseAttrDict.Add(typeof(ThreePeashooter), new CharacterBaseAttr("三发射手", 200, 2, 5, 40));
         }
         public CharacterBaseAttr GetCharacterBaseAttr(Type t)
         {
